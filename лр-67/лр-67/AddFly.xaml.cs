3e0abd49e2d193259fd7b58b77948856b9f93897
sasha <sashakomkova2001@gmail.com>
2021-03-23 19:49:29 +0300

    23/03

--- лр-67/лр-67/AddFly.xaml.cs
+++ лр-67/лр-67/AddFly.xaml.cs
@@ -22,59 +22,7 @@ namespace лр_67
     /// Логика взаимодействия для AddFly.xaml
     /// </summary>
     /// 
-    [Serializable]
-    [XmlRoot(Namespace = "лр_67")]
-    [XmlType("Airline")]
-    public class Airline
-    {
-        [XmlElement(ElementName = "id")]
-        int f_id { get; set; } //автоматически считается
-        [XmlElement(ElementName = "company")]
-        string f_company { get; set; }
-
-        [XmlElement(ElementName = "from")]
-        string f_from { get; set; }
-        [XmlElement(ElementName = "to")]
-        string f_toPoint { get; set; }
-        [XmlElement(ElementName = "price")]
-        int f_price { get; set; }
-        [XmlElement(ElementName = "shortcode")]
-        string f_shortname { get; set; } //состоит из откуда-куда без гласных и в ап
-
-        [XmlElement(ElementName = "fullcode")]
-        string f_fullname { get; set; } //состоит из откуда-куда
-
-        [XmlElement(ElementName = "type")]
-        string f_type { get; set; }
-        [XmlIgnore]
-        BitmapImage image { get; set; }
-
-        public Airline() 
-        {
-            
-        }
-
-        public Airline(
-            int f_id, 
-            string f_company, 
-            string f_from, 
-            string f_toPoint, 
-            int f_price, 
-            string f_shortname,
-            string f_fullname,
-            string f_type
-            )
-        {
-            this.f_id = f_id;
-            this.f_company = f_company;
-            this.f_from = f_from;
-            this.f_toPoint = f_toPoint;
-            this.f_price = f_price;
-            this.f_fullname = f_fullname;
-            this.f_shortname = f_shortname;
-            this.f_type = f_type;
-        }
-    }
+   
     public partial class AddFly : Window
     {
         public AddFly()
@@ -83,20 +31,21 @@ namespace лр_67
         }
 
         public static Airline airline = new Airline();
-        public static List<Airline> airlineList = new List<Airline> { };
+        public static AirlineList airlineList = new AirlineList();
         public static int countAir = 0;
         XmlSerializer formatter = new XmlSerializer(typeof(List<Airline>));
 
         private void LoadButton_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog ofd = new OpenFileDialog();
-            // ofd.Filter = "Image Files(*.BMP, *.PNG, *.JPG, *.JPEG)|*.BMP, *.PNG, *.JPG, *.JPEG|All Files (*.*)|*.*";
-            ofd.Filter = "Image Files(*.bmp, *.PNG, *.jpg, *.JPEG)|*.BMP, *.PNG, *.jpg, *.JPEG|All Files (*.*)|*.*";
+            
+            ofd.Filter = "Фотографии|*.jpg;*.png;*.jpeg| All Files (*.*)|*.*";
             if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK) 
             {
                 try
                 {
                     ImageCompany.Source = new BitmapImage(new Uri(ofd.FileName));
+                    airline.imagePath = ofd.FileName;
                 }
                 catch 
                 {
@@ -104,69 +53,64 @@ namespace лр_67
                 }
             }
         }
-        public string transformStr(string str) 
+        public string transformStr(string str)
         {
-            //string[] voc = { "A", "O", "E", "Y", "I" , "А", "О", "У", "Е", "И", "Я", "Ы", "Э", "Ю", "Ё" };
-            //str.Trim();
-            //str.Replace(" ", "");
-            //for (int i = 0; i < voc.Length; i++)
-            //{
-            //    str.Replace(voc[i], "");
-            //}
-            str.ToUpper();
+            string[] voc = { "A", "O", "E", "Y", "I", "А", "О", "У", "Е", "И", "Я", "Ы", "Э", "Ю", "Ё" };
+            str = str.ToUpper();
+            str = str.Trim();
+            str = str.Replace(" ", "");
+            for (int i = 0; i < voc.Length; i++)
+            {
+                str = str.Replace(voc[i], "");
+                Console.WriteLine(str);
+            }
+
             return str;
         }
-        public string makeShortname(string f_from, string f_to) 
+        public string makeShortname(string f_from, string f_to)
         {
             return $"{transformStr(f_from)}-{transformStr(f_to)}";
         }
         public string makeFullname(string f_from, string f_to)
         {
-            return $"{f_from}-{f_to}";
+            
+            return $"{f_from.ToUpper()}-{f_to.ToUpper()}";
         }
+
         private void Serialize_Click(object sender, RoutedEventArgs e)
         {
-            string s = "s";
-            //airlineList.Add(
-            //    new Airline(
-            //        countAir++,
-            //        CompanyTB.Text,
-            //        FlyFromTB.Text,
-            //        FlyToTB.Text,
-            //        Convert.ToInt32(PriceTB.Text),
-            //        makeShortname(FlyFromTB.Text, FlyToTB.Text),
-            //        makeFullname(FlyFromTB.Text, FlyToTB.Text),
-            //        s
-            //        )
-            //    ); 
+            airline.f_id = countAir++;
+            airline.f_company = CompanyTB.Text;
+            airline.f_from = FlyFromTB.Text;
+            airline.f_toPoint = FlyToTB.Text;
+            airline.f_price = Convert.ToInt32(PriceTB.Text);
+            airline.f_shortname = makeShortname(FlyFromTB.Text, FlyToTB.Text);
+            airline.f_fullname = makeFullname(FlyFromTB.Text, FlyToTB.Text);
+            airlineList.AddItem(airline);
 
-            
-                using (FileStream fs = new FileStream(@"D:\uni\ооп\airlines.xml", FileMode.OpenOrCreate))
-                {
-                    try
-                    {
-                        //formatter.Serialize(fs, airlineList);
-                        System.Windows.MessageBox.Show(
-                        //CompanyTB.Text+
-                        //FlyFromTB.Text+
-                        //FlyToTB.Text+
-                        //Convert.ToInt32(PriceTB.Text)+
-                        makeShortname(FlyFromTB.Text, FlyToTB.Text)//+
-                        //makeFullname(FlyFromTB.Text, FlyToTB.Text)+
-                        //s
-                        );
+             try
+             {
+                Serializer.MyXMLSerializer(airlineList);
+                        //System.Windows.MessageBox.Show(
+                        ////CompanyTB.Text+
+                        ////FlyFromTB.Text+
+                        ////FlyToTB.Text+
+                        ////Convert.ToInt32(PriceTB.Text)+
+                        //makeShortname(FlyFromTB.Text, FlyToTB.Text)+"\n"+
+                        //makeFullname(FlyFromTB.Text, FlyToTB.Text)
+                         //);
 
-                    }
-                    catch
-                    {
-                        System.Windows.MessageBox.Show("Что-то пошло не так..");
-                    }
-                    finally
-                    {
-                        System.Windows.MessageBox.Show("Данные записаны в файл");
-                    }
-                    fs.Close();
-                }
+             }
+             catch
+             {
+                System.Windows.MessageBox.Show("Что-то пошло не так..");
+             }
+             finally
+             {
+                System.Windows.MessageBox.Show("Данные записаны в файл");
+             }
+                   
+              
             
             
         }
---
+++ лр-67/лр-67/Airline.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using System.Xml.Serialization;
+
+namespace лр_67
+{
+    [Serializable]
+    [XmlRoot(Namespace = "лр_67")]
+    [XmlType("Airline")]
+    public class Airline
+    {
+        [XmlElement(ElementName = "id")]
+        public int f_id { get; set; } //автоматически считается
+        [XmlElement(ElementName = "company")]
+        public string f_company { get; set; }
+
+        [XmlElement(ElementName = "from")]
+        public string f_from { get; set; }
+        [XmlElement(ElementName = "to")]
+        public string f_toPoint { get; set; }
+        [XmlElement(ElementName = "price")]
+        public int f_price { get; set; }
+        [XmlElement(ElementName = "shortcode")]
+        public string f_shortname { get; set; } //состоит из откуда-куда без гласных и в ап
+
+        [XmlElement(ElementName = "fullcode")]
+        public string f_fullname { get; set; } //состоит из откуда-куда
+
+        [XmlIgnore]
+        public string imagePath { get; set; }
+
+        public Airline()
+        {
+
+        }
+
+        public Airline(
+            int f_id,
+            string f_company,
+            string f_from,
+            string f_toPoint,
+            int f_price,
+            string f_shortname,
+            string f_fullname,
+            string imagePath
+            )
+        {
+            this.f_id = f_id;
+            this.f_company = f_company;
+            this.f_from = f_from;
+            this.f_toPoint = f_toPoint;
+            this.f_price = f_price;
+            this.f_fullname = f_fullname;
+            this.f_shortname = f_shortname;
+            this.imagePath = imagePath;
+        }
+        public Airline Clone()
+        {
+            return new Airline(
+                this.f_id, 
+                this.f_company, 
+                this.f_from, 
+                this.f_toPoint, 
+                this.f_price, 
+                this.f_fullname, 
+                this.f_shortname,
+                this.imagePath);
+        }
+    }
+    public class AirlineList
+    {
+        [XmlArray("Collection"), XmlArrayItem("Airline")]
+        public List<Airline> list { get; set; }
+        public AirlineList()
+        {
+            list = new List<Airline> { };
+        }
+
+        public void AddItem(Airline obj)
+        {
+            list.Add(obj.Clone());
+        }
+
+        //public Food Last()
+        //{
+        //    return list[list.Count - 1];
+        //}
+    }
+    public static class Serializer
+    {
+        public static void MyXMLSerializer<T>(T obj)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(AirlineList));
+            using (FileStream fs = new FileStream($"airlines.xml", FileMode.Create))
+            {
+                xmlSerializer.Serialize(fs, obj);
+                fs.Close();
+            }
+
+        }
+        public static AirlineList MyXMLDeserializer()
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(AirlineList));
+            using (FileStream fs = new FileStream($"airlines.xml", FileMode.OpenOrCreate))
+            {
+                var restObj = xmlSerializer.Deserialize(fs) as AirlineList;
+                fs.Close();
+                return (restObj);
+            }
+        }
+    }
+}
